913d75b54dbf77107f01deb8d6c4a4a27ae535da
przemek9300 <przemek930010@gmail.com>
2017-04-21 20:29:17 +0200

    new ctor to Post
    Signed-off-by: przemek9300 <przemek930010@gmail.com>

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,7 +1,43 @@
+using System;
+using System.Collections.Generic;
+
 namespace Projekt.Models
 {
     public class Post
     {
-        
+        public Guid Id
+        { get; set; }
+ 
+        public string Title
+        { get; set; }
+ 
+        public string Description
+        { get; set; }
+ 
+        public string Content
+        { get; set; }
+ 
+        public string UrlSlug
+        { get; set; }
+ 
+        public DateTime PostedOn
+        { get; set; }
+ 
+        public DateTime Modified
+        { get; set; }
+ 
+        public  IList<Tag> Tags
+        { get; set; }
+
+
+        public Post(string Title, string Description, string Content)
+        {   
+            this.Id=new Guid();
+            this.Title=Title;
+            this.Description=Description;
+            this.Content=Content;
+            this.PostedOn=DateTime.Now;
+
+        }
     }
 }
